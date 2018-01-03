0ad51a7b575db036eb31a2fa661aaa5a54faed70
Pakapon Kaewmark <sleep191@gmail.com>
2018-01-03 21:47:21 +0700

    refactor: Setup the project to use Redux, React Router and Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+	<div className="container">
+		<h1>Readable Project</h1>
+
+		<Switch>
+			<Route exact path="/" component={() => (
+				<h2>Posts</h2>
+			)} />
+		</Switch>
+	</div>
+)
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import createHistory from 'history/createBrowserHistory'
+
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+
+import reducers from './reducers'
+
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+	combineReducers({
+		...reducers,
+		router: routerReducer
+	}),
+	applyMiddleware(middleware)
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
+	</Provider>,
+	document.getElementById('root')
+);
+
 registerServiceWorker();
