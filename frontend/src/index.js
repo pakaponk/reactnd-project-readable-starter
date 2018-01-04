5e8d3a8277122d053bd200b90b0b6edd11b622f5
Pakapon Kaewmark <sleep191@gmail.com>
2018-01-04 13:01:37 +0700

    refactor: Setup Redux thunk middleware and Redux Dev Tool

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import createHistory from 'history/createBrowserHistory'
 
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+import thunkMiddleware from 'redux-thunk'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome/styles.css'
 
 import reducers from './reducers'
 
-import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -21,14 +24,18 @@ const history = createHistory()
 // Build the middleware for intercepting and dispatching navigation actions
 const middleware = routerMiddleware(history)
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // Add the reducer to your store on the `router` key
 // Also apply our middleware for navigating
 const store = createStore(
-	combineReducers({
-		...reducers,
-		router: routerReducer
-	}),
-	applyMiddleware(middleware)
+	reducers,
+	composeEnhancers(
+		applyMiddleware(
+			thunkMiddleware,
+			middleware
+		)
+	)
 )
 
 ReactDOM.render(
